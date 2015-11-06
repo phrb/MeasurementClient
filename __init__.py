3599b0f1cdd37778a9bb767737a4d66dda6208cb
phrb <pedro.bruel@gmail.com>
2015-11-06 20:34:40 -0200

    Add interface.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+import GCEInterface.interface
+import GCEInterface.server_codes
+import client
+
+from GCEInterface.interface import GCEInterface
+from GCEInterface.server_codes import *
+from client import MeasurementClient
