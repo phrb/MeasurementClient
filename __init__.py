8fa1516f9fdad8302852d29e2faf026ab2d317c7
phrb <pedro.bruel@gmail.com>
2015-11-06 22:03:43 -0200

    Change imports.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 import gce_interface.interface
 import gce_interface.server_codes
+
 import client
 
 from gce_interface.interface import GCEInterface
 from gce_interface.server_codes import *
+from client import MeasurementClient
 
 def argparsers():
     return [
